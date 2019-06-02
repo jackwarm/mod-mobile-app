be48553957042ee33e3654d10cb78d386adbf3b9
jackwarm <john@cieply.com>
2019-06-02 14:19:52 -0400

    Reorganize

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import EditCard from './components/EditCard';
 import DisplayAllCards from './components/DisplayAllCards';
 import LoadingDisplay from './components/LoadingDisplay';
 import './style.css';
-//import iconEdit from "./icon-edit.svg";
-//import logo from './logo.svg';
 
 const initialUserLength = 5;
 const APIurl = 'https://randomuser.me/api/?results=';
@@ -137,8 +135,6 @@ class App extends Component {
     }
     return (
       <div>
-        <img src='{iconEdit}' />
-        <img src={logo} className="App-logo" alt="logo" />
         <RefreshList currentSize={this.state.maxUserList} goRefresh={this.goRefresh} />
         <SearchCards goSearch={this.goSearch} resetSearch={this.resetSearch} />
         <SortCards goSort={this.goSort} />
